1af4af83f6113b80aaa1adcb3f5ba5039cd6bd8c
xjin <xjinhz@126.com>
2018-12-19 23:22:49 +0800

    second commit,learn to page 55(2.3)

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post   # 是 from .models
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+
+admin.site.register(Post, PostAdmin)
